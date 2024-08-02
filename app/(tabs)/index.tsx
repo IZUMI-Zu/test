9170b62f9eda05b5b99c2e6b7da38008d15ad082
binshuoz <124227335+binshuoz@users.noreply.github.com>
2024-08-02 21:13:31 +0800

    Test (#7)
    * Update useColorScheme.ts
    
    * Update useColorScheme.ts
    
    * Update useColorScheme.ts
    
    * test
    
    * test
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Update explore.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -19,6 +19,9 @@ export default function HomeScreen() {
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Test!</ThemedText>
+      </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
